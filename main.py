5cd97407f8315106fd9d725ea1501ad49ffa2e0e
Timur Shmarakhmanov <t.shamsrakhmanov@gmail.com>
2024-10-23 15:43:19 +0400

    base post implementation

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class BalanceSheet(BaseModel):
+    rqUID: str
+    clientId: int
+    account: int
+    openDate: str
+    closeDate: str
 
 
 def make_fastapi_application() -> FastAPI:
@@ -7,6 +16,14 @@ def make_fastapi_application() -> FastAPI:
     @application.get("/about")
     async def about():
         return {"Тестовая заглушка для закрытия карточки ИПР1"}
+
+    @application.post("/get_balance/")
+    async def get_balance(balance_sheet: BalanceSheet):
+        answer = dict()
+
+        print(balance_sheet)
+
+        return answer
 
     return application
 
